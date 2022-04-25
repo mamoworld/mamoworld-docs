5c6fb5856cf963fe8fef5d1e6c8e5df578109ba1
Mathias Möhl <info@mamoworld.com>
2022-04-25 16:50:30 +0200

    added square option to YouTube tag and worked on more docs

--- src/components/YouTube.js
+++ src/components/YouTube.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 function Youtube(props){ 
-	return <div className="video-container"><iframe src={'https://www.youtube.com/embed/'+ props.id} frameborder="0" allowfullscreen="allowfullscreen"></iframe></div>
+	return props.square ? 
+	<div className="video-container square"><iframe src={'https://www.youtube.com/embed/'+ props.id} frameborder="0" allowfullscreen="allowfullscreen"></iframe></div>
+	:
+	<div className="video-container"><iframe src={'https://www.youtube.com/embed/'+ props.id} frameborder="0" allowfullscreen="allowfullscreen"></iframe></div>
 }
 export default Youtube; 
