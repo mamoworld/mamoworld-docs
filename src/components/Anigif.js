23ffc5354bdea4d5561ab4044f5f8edc468f359f
Mathias Möhl <info@mamoworld.com>
2022-05-10 20:20:24 +0200

    fixed issues with freezeframe not working anymore after build

--- src/components/Anigif.js
+++ src/components/Anigif.js
@@ -1,10 +1,23 @@
 import React, { useState } from 'react';
 import _uniqueId from 'lodash/uniqueId';
-
-function Anigif(props){ 
-	const [id] = useState(_uniqueId('prefix-'));
+import BrowserOnly from '@docusaurus/BrowserOnly';
+// function Anigif(props){ 
+// 	const [id] = useState(_uniqueId('prefix-'));
 	  
-	return <div style={{marginBottom: "20px", maxWidth:props.width}}><img   className="freezeframe" id={id} src={props.src} onLoad={function(){new Freezeframe({selector:'#'+id,  trigger:'click', overlay:true, responsive:true})}} /></div>;};
+// 	return <div  style={{marginBottom: "20px", maxWidth:props.width}}><img  id={id} className="freezeframe"  src={props.src} onLoad={function(){new Freezeframe({selector:'#'+id,  trigger:'click', overlay:true, responsive:true})}} /></div>;};
+
+
+class Anigif extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+		  id: _uniqueId('prefix-')
+		};
+	  }
+	render() {
+		return <div  style={{marginBottom: "20px", maxWidth:this.props.width}}><img  id={this.state.id} className="freezeframe"  src={this.props.src} onLoad={function(e){new Freezeframe({selector:e.target,  trigger:'click', overlay:true, responsive:true})}} /></div>;
+	}
+  }
 
 export default Anigif; 
 	
