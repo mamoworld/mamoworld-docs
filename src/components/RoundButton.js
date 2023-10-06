58d0828681252376d06654e8fcc19628f311f44b
Mathias Moehl <info@mamoworld.com>
2023-10-06 18:49:59 +0200

    documented more tools

--- src/components/RoundButton.js
+++ src/components/RoundButton.js
@@ -4,20 +4,26 @@ import useBaseUrl from '@docusaurus/useBaseUrl';
 const RoundButton = ({ label, url, blank = false }) => {
   const finalUrl = useBaseUrl(url);
 
+  // Check if the url prop is provided
+  const isDisabled = !url;
   return (
-    <div style={{marginBottom:'20px'}}><a href={finalUrl} 
-                        target={blank ? '_blank' : '_self'} 
-                        rel={blank ? 'noopener noreferrer' : undefined} // Add noopener noreferrer only if blank is true
-                        style={{
-                        backgroundColor: '#f3971b',
-                        borderRadius: '20px',
-                        color: '#fff',
-                        padding: '10px 17px 10px 17px',
-                        cursor: 'pointer',
-                    }}
-        className="round-button">
-      {label}
-    </a>
+    <div style={{marginBottom:'20px'}}><a
+        href={isDisabled ? undefined : finalUrl}
+        target={blank && !isDisabled ? '_blank' : '_self'}
+        rel={blank && !isDisabled ? 'noopener noreferrer' : undefined}
+        style={{
+          backgroundColor: isDisabled ? '#ccc' : '#f3971b',
+          borderRadius: '20px',
+          color: '#fff',
+          padding: '10px 17px 10px 17px',
+          cursor: isDisabled ? 'not-allowed' : 'pointer',
+        }}
+        className="round-button"
+        // Disable the button when there is no URL
+        disabled={isDisabled}
+      >
+        {label}
+      </a>
     </div>
   );
 };
